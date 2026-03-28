68726a5e394745bcd7c78b957a350c53f9f6cf23
Mohamad Hamsho <hamsho1030@gmail.com>
2026-03-28 11:09:02 -0400

    rendered movie list on webpage

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-import { moviesData } from "./data";
+import { moviesData } from "./data.js";
 
 const moviesEl = document.getElementById('movie-lst');
 
 
 function renderList(){
     const renderList = movieList()
+    let html = ""
+
+    for(let genres of renderList){
+        html += `
+        <div class="movieRendered">
+            <label for="${genres}">${genres}</label>
+            <input type="radio" id="${genres}" name="genres" value="${genres }">
+        </div>
+    `
+    }
+    moviesEl.innerHTML = html
 }
 
 function movieList(){
     const movieArr = []
     for(let movie of moviesData){
-        for(let genres of movie.genreTags){
-            if(!movieArr.includes(genres))
-                movieArr.push(genres)
+        for(let genre of movie.genreTags){
+            if(!movieArr.includes(genre))
+                movieArr.push(genre)
         }
     }
     return movieArr
 }
+
+
+renderList()
