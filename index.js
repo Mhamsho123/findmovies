b36b2d001f0a80feb92dac5e355c01742dabde82
Mohamad Hamsho <hamsho1030@gmail.com>
2026-03-29 16:56:20 -0400

    Added functionality to clicking to the radio and using a click to check your choice

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 import { moviesData } from "./data.js";
 
 const moviesEl = document.getElementById('movie-lst');
+const submitEl = document.getElementById('submit-btn')
+const renderMovie = document.getElementById('rendered-movie')
+
+
+
+moviesEl.addEventListener("change", selectedMovieGenre)
+submitEl.addEventListener('click', renderedMovie)
+
+
+function selectedMovieGenre(e){
+    const movieRendered = document.getElementsByClassName('movieRendered')
+    for(let movie of movieRendered){
+        movie.classList.remove('highlight')
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+    
+}
+function movieObjectArray(){
+    let movieChoice = document.querySelector('input[type="radio"]:checked')
+    console.log(movieChoice)
+}
+
+
+
+function renderedMovie(){
+    const movieSelected = movieObjectArray()
+}
 
 
 function renderList(){
